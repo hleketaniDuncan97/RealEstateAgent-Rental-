297541ff6b9f4dfef62fa97707ec6fb0bf306764
yashilBBD <yashil.luckan@bbd.co.za>
2024-07-07 18:32:44 +0200

    Added Auth to backed

--- servers/admin/src/routes/lease.routes.ts
+++ servers/admin/src/routes/lease.routes.ts
@@ -5,11 +5,12 @@ import {
 } from '../controllers/lease.controllers'
 import { Request } from '../schemas/lease.schemas'
 import validator from '../middleware/validator'
+import authenticateToken from "../middleware/auth";
 
 const router = Router()
 
 router.route('/')
-  .get(validator(Request.Query.FetchLeases, 'query'), fetchLeases)
+  .get(authenticateToken, validator(Request.Query.FetchLeases, 'query'), fetchLeases)
   // .post(validator(Request.Body.CreateLease, 'body'), createLase)
 
 export default router
